ef3d7b7afb3306aa817fca42956158bc5564619e
Sadikul Islam <53344443+TheKing205@users.noreply.github.com>
2019-09-12 03:28:01 +0600

    Create Script.py

---
+++ Script.py
@@ -0,0 +1,26 @@
+import requests
+import time
+print('''
+========================================================================
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+========================================================================
+           www.facebook.com/sadikul.official || Never Give UP 
+========================================================================
+''')
+file = str(input("Enter Your File Name: ")).strip()
+print("[=] Loading Please wait...")
+print("\n")
+time.sleep(3)
+with open(file , "r") as urls:
+    for url in urls:
+        print(url.strip())
+        response = requests.get(url.strip())
+        if response.status_code == 200:
+            print("[=] Status 200 ==> OK")
+            save = open("Live.txt" , "a").writelines(url)
+            print("[=] Saved !!!")
+            print("\n")
+        else:
+            print("[=] 404 Not Found")
+            print("\n")
+input("[+] Press Enter To Exit")
